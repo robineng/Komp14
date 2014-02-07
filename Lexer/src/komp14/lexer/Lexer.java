1cf41ce821324dd34fe688fe483b45781ca40c18
Robin Engström <roen@kth.se>
2014-02-07 13:55:34 +0100

    Initial commit

---
+++ Lexer/src/komp14/lexer/Lexer.java
@@ -0,0 +1,17 @@
+package komp14.lexer;
+
+public class Lexer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if(args.length != 1){
+			System.err.println("Input valid filename");
+			return;
+		}
+		
+
+	}
+
+}
