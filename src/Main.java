494e826caa89b5b3e55b5aa8e077222e125c5a8b
Casper Winsnes <cwinsnes92@gmail.com>
2014-03-18 13:31:54 +0100

    Make mainclass work as intended.

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,28 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import java.io.FileInputStream;
+import java.io.IOError;
+import java.io.IOException;
+
 /**
  * Created by suicidal on 3/18/14.
  */
 public class Main {
     public static void main(String[] args) {
-        javagrammarLexer lexer = new javagrammarLexer(new ANTLRInputStream(args[0]));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        try {
+            javagrammarLexer lexer = new javagrammarLexer(new ANTLRInputStream(new FileInputStream(args[0])));
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-        javagrammarParser parser = new javagrammarParser(tokens);
-        javagrammarParser.ProgramContext context = parser.program();
+            javagrammarParser parser = new javagrammarParser(tokens);
+            javagrammarParser.ProgramContext context = parser.program();
 
-        ParseTreeWalker walker = new ParseTreeWalker();
-        javagrammarSymbolListener listener new javagrammarSymbolListener();
-        walker.walk(listener, context);
+            ParseTreeWalker walker = new ParseTreeWalker();
+            javagrammarSymbolListener listener = new javagrammarSymbolListener();
+            walker.walk(listener, context);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
