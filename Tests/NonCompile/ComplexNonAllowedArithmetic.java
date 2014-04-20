5bfdbf6ed76e5900b0c134980c3231bd88bf9698
Casper Winsnes <cwinsnes92@gmail.com>
2014-04-20 14:45:51 +0200

    some test cases

---
+++ Tests/NonCompile/ComplexNonAllowedArithmetic.java
@@ -0,0 +1,5 @@
+class ComplexNonAllowedArithmetic {
+    public static void main(String[] args) {
+        
+    }
+}
--- testfile.java
+++ testfile.java
@@ -7,12 +7,27 @@ class TestingVariables {
     int x;
     int y;
 
-    public int toTryParamsWith(int x, int y) {
+    public int toTryParamsWith(int x, int y, boolean z) {
         return x;
     }
 
     public int tryWithWeirdParams() {
         int y;
-        return this.toTryParamsWith(x,y);
+        return this.toTryParamsWith(x,y,true);
+    }
+
+    public int tryOtherWeirdParams() {
+        boolean b;
+        if(x < y && y > x && b && b) {
+            System.out.println(x < y && y < b);
+            //System.out.println(x);
+        }
+        return 3;
+    }
+}
+
+class otherclass {
+    public boolean functionThatDoesNotReturnInt() {
+        return true;
     }
 }
