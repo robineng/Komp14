810405c396946b0b68dd49f3c11637a85c0426c7
Robin Engström <roen@kth.se>
2014-04-23 14:55:59 +0200

    Casp storkodar

--- src/mjc/JVMMain.java
+++ src/mjc/JVMMain.java
@@ -1,11 +1,15 @@
 package mjc;
 
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.misc.Nullable;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.BitSet;
 import java.util.HashMap;
 
 /**
@@ -15,6 +19,30 @@ public class JVMMain {
     public static void main(String[] args) {
         try {
             javagrammarLexer lexer = new javagrammarLexer(new ANTLRInputStream(new FileInputStream(args[0])));
+            lexer.addErrorListener(new ANTLRErrorListener() {
+                @Override
+                public void syntaxError(@NotNull Recognizer<?, ?> recognizer, @Nullable Object o, int i, int i2, @NotNull String s, @Nullable RecognitionException e) {
+                    System.err.println("Syntax error!");
+                    System.err.println(s);
+                    System.exit(1);
+                }
+
+                @Override
+                public void reportAmbiguity(@NotNull Parser parser, @NotNull DFA dfa, int i, int i2, boolean b, @Nullable BitSet bitSet, @NotNull ATNConfigSet atnConfigs) {
+                    System.err.println(String.format("ambiguity at: %d %d\n", i, i2));
+                    System.exit(1);
+                }
+
+                @Override
+                public void reportAttemptingFullContext(@NotNull Parser parser, @NotNull DFA dfa, int i, int i2, @Nullable BitSet bitSet, @NotNull ATNConfigSet atnConfigs) {
+
+                }
+
+                @Override
+                public void reportContextSensitivity(@NotNull Parser parser, @NotNull DFA dfa, int i, int i2, int i3, @NotNull ATNConfigSet atnConfigs) {
+
+                }
+            });
             CommonTokenStream tokens = new CommonTokenStream(lexer);
 
             javagrammarParser parser = new javagrammarParser(tokens);
--- testfile.java
+++ testfile.java
@@ -1,14 +1,5 @@
-class multidim {
+class Göö {
   public static void main(String[] args) {
-    long[] asdf;
-    int[] kek;
-    int i;
-    long l;
-
-    asdf = new long[5];
-      l = asdf[true];
-      kek = new int[true];
-      i = kek[true];
 
   }
 }
